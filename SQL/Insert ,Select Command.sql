b46dd8565b7a292d4e348f6e0e825e32a1aafa2d
Mahima-Barde <mahimabarde2001@gmail.com>
2023-06-04 13:01:30 +0530

    file added

---
+++ SQL/Insert ,Select Command.sql
@@ -0,0 +1,13 @@
+select  * from stud1;
+insert into stud1 values(2,'Amit','Science'),
+insert into stud1 values(3,'Meher','Chemistry'),
+(4,'Tanu','Physics');
+insert into stud1 values(5,'Nidhi','Maths'),
+(6,'Manij','Science'),
+(7,'Ramesh','Physics'),
+(8,'Ishika','Social science'),
+(9,'Taniya','Science'),
+(10,'Mahima','English'),
+(11,'Rupali','Social Science');
+select * from stud1;
+
